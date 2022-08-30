6d1155f5b3b850d9ea6d4dba5d7e2009f8302b6d
Kareem Ali <kareem.ali.rahman@gmail.com>
2022-08-30 07:33:08 +0200

    add generate token function

--- packages/api/src/authentication/jwt.ts
+++ packages/api/src/authentication/jwt.ts
@@ -1,4 +1,5 @@
 import prisma from "../../prisma/primsa-client";
+import * as jwt from "jsonwebtoken";
 
 const map = new Map<string, string>();
 map.set("666", "kareem@ali.com");
@@ -12,4 +13,10 @@ export const decodeAndVerifyJwtToken = async (token: string) => {
         }
     });
     return user;
+}
+
+export const generateAccessToken = (email: string) => {
+    if(!process.env.TOKEN_SECRET) throw new Error('no secret found in environment variables');
+    const token = jwt.sign(email, process.env.TOKEN_SECRET, { expiresIn: '1800s' })
+    return token;
 }
