c25ef4a2f3869d9c4d3b6276fe3ddc6b4ae42754
Kareem Ali <kareem.ali.rahman@gmail.com>
2022-08-30 07:17:16 +0200

    fix authorization

--- packages/api/src/authentication/jwt.ts
+++ packages/api/src/authentication/jwt.ts
@@ -5,6 +5,7 @@ map.set("666", "kareem@ali.com");
 
 export const decodeAndVerifyJwtToken = async (token: string) => {
     const email = map.get(token);
+    if(!email)return null;
     const user = await prisma.user.findFirst({
         where: {
             email : email
